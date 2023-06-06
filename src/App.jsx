db74212de35ecc16b4c79fd57c4ec1fe9c67be3b
jasonpieroni <jasonpieroni@gmail.com>
2023-06-06 16:38:43 -0400

    added files to main branch

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    
+       <nav>
+            <img src="/public/boston-red-sox-logo-transparent.png" className="redsox-image" />
+            <h1 className="nav-title">Red Sox Fan Page</h1>
+            <h4 className="nav-link">Team Roster</h4>
+        </nav>
+  
+  )
+}
+
+export default App
